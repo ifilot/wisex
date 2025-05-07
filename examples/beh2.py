Fixing calculation of R2 metric

examples/beh2.py
@@ -20,7 +20,7 @@ localizer.perform_localization(method='fosterboys')
 outpath = os.path.join(os.path.dirname(__file__), 'output', 'beh2')
 os.makedirs(outpath, exist_ok=True)
 geodesic = Geodesic(localizer.u_opt, nocc=localizer.nocc)
-tt = np.linspace(0, 1, 10)
+tt = np.linspace(0, 1, 20)
 
 # geodesic.build_generator(group='su(n)')
 # for i,t in enumerate(tt):

examples/bh3.py
@@ -18,7 +18,7 @@ localizer.perform_localization(method='fosterboys')
 #localizer.report_matrix()
 #localizer.show_jacobi_rotations()
 
-tt = np.linspace(0, 1, 10)
+tt = np.linspace(0, 1, 20)
 outpath = os.path.join(os.path.dirname(__file__), 'output', 'bh3')
 os.makedirs(outpath, exist_ok=True)
 geodesic = Geodesic(localizer.u_opt, nocc=localizer.nocc)

examples/ethylene.py
@@ -1,6 +1,7 @@
 import os, sys
 from pyqint import Molecule
 import numpy as np
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from wisex import Localizer, Geodesic
@@ -22,7 +23,7 @@ localizer.report_matrix()
 outpath = os.path.join(os.path.dirname(__file__), 'output', 'ethylene')
 os.makedirs(outpath, exist_ok=True)
 geodesic = Geodesic(localizer.u_opt, nocc=localizer.nocc)
-tt = np.linspace(0, 1, 10)
+tt = np.linspace(0, 1, 20)
 
 # geodesic.build_generator(group='su(n)')
 # for i,t in enumerate(tt):

wisex/localizer.py
@@ -96,8 +96,14 @@ class Localizer:
         else:
             plt.show()
     
-    def calculate_fbr2(self, orbc, nocc):
-        return np.sum(np.einsum('pi,qi,pql->il', orbc[:,:nocc], orbc[:,:nocc], self.dipolmat)**2)
+    def calculate_fbr2(self, U, nocc):
+        """
+        Given a unitary transformation matrix, determine the R2 value
+        """
+        return np.sum(np.einsum('pi,qi,pql->il', 
+                                (self.data['orbc'] @ U)[:,:nocc], 
+                                (self.data['orbc'] @ U)[:,:nocc], 
+                                self.dipolmat)**2)
 
     def show_jacobi_rotations(self, figsize=(16,16)):
         nsteps = len(self.screenarr)